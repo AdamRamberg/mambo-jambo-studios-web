3c565481d6cdfe200b9c6e2d3a5966c15cf21a2f
Adam Ramberg <andersson.adam.89@gmail.com>
2018-08-26 21:06:52 +0200

    Hamburger explicit close sequence

--- src/components/Hamburger/styles.js
+++ src/components/Hamburger/styles.js
@@ -3,50 +3,95 @@ import styled, { keyframes } from 'styled-components';
 const TRANSLATE_TO = '12px';
 
 const smooth = [
-  [
-    { percent: 0, transform: `translateY(0px);` },
-    { percent: 25, transform: `translateY(${TRANSLATE_TO});` },
-    { percent: 100, transform: `translateY(${TRANSLATE_TO}) rotateZ(-45deg);` },
-  ],
-  [
-    { percent: 0, transform: `scaleX(1);` },
-    { percent: 100, transform: `scaleX(0.1);` },
-  ],
-  [
-    { percent: 0, transform: `translateY(0px);` },
-    { percent: 25, transform: `translateY(${`-${TRANSLATE_TO}`});` },
-    {
-      percent: 100,
-      transform: `translateY(${`-${TRANSLATE_TO}`}) rotateZ(45deg);`,
-    },
-  ],
+  {
+    open: [
+      { percent: 0, transform: `translateY(0px);` },
+      { percent: 25, transform: `translateY(${TRANSLATE_TO});` },
+      {
+        percent: 100,
+        transform: `translateY(${TRANSLATE_TO}) rotateZ(-45deg);`,
+      },
+    ],
+  },
+  {
+    open: [
+      { percent: 0, transform: `scaleX(1);` },
+      { percent: 100, transform: `scaleX(0.1);` },
+    ],
+  },
+  {
+    open: [
+      { percent: 0, transform: `translateY(0px);` },
+      { percent: 25, transform: `translateY(${`-${TRANSLATE_TO}`});` },
+      {
+        percent: 100,
+        transform: `translateY(${`-${TRANSLATE_TO}`}) rotateZ(45deg);`,
+      },
+    ],
+  },
 ];
 
-// START HERE: Make option to explicitly define animations both ways
 const elastic = [
-  [
-    { percent: 0, transform: `translateY(0px);` },
-    { percent: 10, transform: `translateY(${TRANSLATE_TO});` },
-    { percent: 90, transform: `translateY(${TRANSLATE_TO}) rotateZ(-50deg);` },
-    { percent: 100, transform: `translateY(${TRANSLATE_TO}) rotateZ(-45deg);` },
-  ],
-  [
-    { percent: 0, transform: `scaleX(1);` },
-    { percent: 90, transform: `scaleX(0.05);` },
-    { percent: 100, transform: `scaleX(0.1);` },
-  ],
-  [
-    { percent: 0, transform: `translateY(0px);` },
-    { percent: 10, transform: `translateY(${`-${TRANSLATE_TO}`});` },
-    {
-      percent: 90,
-      transform: `translateY(${`-${TRANSLATE_TO}`}) rotateZ(50deg);`,
-    },
-    {
-      percent: 100,
-      transform: `translateY(${`-${TRANSLATE_TO}`}) rotateZ(45deg);`,
-    },
-  ],
+  {
+    open: [
+      { percent: 0, transform: `translateY(0px);` },
+      { percent: 10, transform: `translateY(${TRANSLATE_TO});` },
+      {
+        percent: 90,
+        transform: `translateY(${TRANSLATE_TO}) rotateZ(-55deg);`,
+      },
+      {
+        percent: 100,
+        transform: `translateY(${TRANSLATE_TO}) rotateZ(-45deg);`,
+      },
+    ],
+    close: [
+      {
+        percent: 0,
+        transform: `translateY(${TRANSLATE_TO}) rotateZ(-45deg);`,
+      },
+
+      { percent: 30, transform: `translateY(${TRANSLATE_TO}) rotateZ(0deg);` },
+      { percent: 90, transform: `translateY(-4px) rotateZ(0deg);` },
+      { percent: 100, transform: `translateY(0px) rotateZ(0deg);` },
+    ],
+  },
+  {
+    open: [
+      { percent: 0, transform: `scaleX(1);` },
+      { percent: 90, transform: `scaleX(0.05);` },
+      { percent: 100, transform: `scaleX(0.1);` },
+    ],
+    close: [
+      { percent: 0, transform: `scaleX(0.1);` },
+      { percent: 90, transform: `scaleX(1.1);` },
+      { percent: 100, transform: `scaleX(1);` },
+    ],
+  },
+  {
+    open: [
+      { percent: 0, transform: `translateY(0px);` },
+      { percent: 10, transform: `translateY(${`-${TRANSLATE_TO}`});` },
+      {
+        percent: 90,
+        transform: `translateY(${`-${TRANSLATE_TO}`}) rotateZ(55deg);`,
+      },
+      {
+        percent: 100,
+        transform: `translateY(${`-${TRANSLATE_TO}`}) rotateZ(45deg);`,
+      },
+    ],
+    close: [
+      {
+        percent: 0,
+        transform: `translateY(-${TRANSLATE_TO}) rotateZ(45deg);`,
+      },
+
+      { percent: 30, transform: `translateY(-${TRANSLATE_TO}) rotateZ(0deg);` },
+      { percent: 90, transform: `translateY(4px) rotateZ(0deg);` },
+      { percent: 100, transform: `translateY(0px) rotateZ(0deg);` },
+    ],
+  },
 ];
 
 const ANIMATION_TYPES = {
@@ -55,19 +100,38 @@ const ANIMATION_TYPES = {
 };
 
 const createKeyframes = ({ index, open, animationType }) => {
+  if (!Object.prototype.hasOwnProperty.call(ANIMATION_TYPES, animationType)) {
+    // eslint-disable-next-line no-console
+    console.warn(
+      `Animation type ${animationType} not supported. Supported types are: ${Object.keys(
+        ANIMATION_TYPES,
+      ).join(', ')}. Defaulting to smooth.`,
+    );
+    animationType = 'smooth'; // eslint-disable-line no-param-reassign
+  }
+
+  const hasExplicitCloseSeq = Object.prototype.hasOwnProperty.call(
+    ANIMATION_TYPES[animationType][index],
+    'close',
+  );
   const sequence =
-    (ANIMATION_TYPES[animationType] && ANIMATION_TYPES[animationType][index]) ||
-    smooth[index];
+    !open && hasExplicitCloseSeq
+      ? ANIMATION_TYPES[animationType][index].close
+      : ANIMATION_TYPES[animationType][index].open;
 
   const stringifiedSeq = sequence.map(
     step =>
-      `${open ? step.percent : 100 - step.percent}% { transform: ${
-        step.transform
-      } }`,
+      `${
+        open || hasExplicitCloseSeq ? step.percent : 100 - step.percent
+      }% { transform: ${step.transform} }`,
   );
 
   return keyframes`
-    ${open ? stringifiedSeq.join(' ') : stringifiedSeq.reverse().join(' ')}
+    ${
+      open || hasExplicitCloseSeq
+        ? stringifiedSeq.join(' ')
+        : stringifiedSeq.reverse().join(' ')
+    }
   `;
 };
 
